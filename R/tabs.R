5c0641d0336528bc69e11a6265c54b22dd57a292
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2019-01-31 05:06:28 -0800

    add default display_tab

--- R/tabs.R
+++ R/tabs.R
@@ -1,5 +1,5 @@
 # Generate Tab Modules
-module_tabs <- function(tabs, display_tab){
+module_tabs <- function(tabs, display_tab = function(x){TRUE}){
   function(input, output, session, data_global, ...){
     ns <- session$ns
     tabs %>%
@@ -11,9 +11,7 @@ module_tabs <- function(tabs, display_tab){
             callModule(.fun, tabName, data_global = data_global)
           }
         }
-        print(.$text)
         if (!display_tab(.$text)){
-          print("Appending tab...")
           appendTab('tab', tabPanel(.$text))
         }
       })
@@ -26,11 +24,11 @@ module_tabs_ui <- function(tabs, display_tab){
     tabs %>%
       purrr::keep(~ display_tab(.$text)) %>%
       purrr::map(~ {
-          tabName <- make_tab_name(.)
-          .fun_ui <- get_module_ui(.)
-          shiny::tabPanel(
-            .$text, .fun_ui(ns(tabName), data_global = data_global)
-          )
+        tabName <- make_tab_name(.)
+        .fun_ui <- get_module_ui(.)
+        shiny::tabPanel(
+          .$text, .fun_ui(ns(tabName), data_global = data_global)
+        )
       }) %>%
       append(list(id = ns('tab'))) %>%
       do.call(shiny::tabsetPanel, .)
