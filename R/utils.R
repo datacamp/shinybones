5593ce04c935a7e316ac0fdf0e00995519fccf8d
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2019-01-31 05:07:01 -0800

    add function to scaffold rstudio proj

--- R/utils.R
+++ R/utils.R
@@ -35,7 +35,7 @@ default_placeholder_ui <- function(title, text) {
   function(id, ...) {
     ns <- NS(id)
     shiny::fluidRow(
-      shinydashboard::box(
+      shinydashboard::box(width = 12,
         title = title,
         span(class = "text-danger", text),
         solidHeader = TRUE
@@ -47,10 +47,24 @@ default_placeholder_ui <- function(title, text) {
 placeholder_ui <-  function(id, title){
   ns <- shiny::NS(id)
   shiny::fluidRow(
-    shinydashboard::box(
+    shinydashboard::box(width = 12,
       title = title,
       span(class = 'text-muted', 'This is a placeholder'),
       solidHeader = TRUE
     )
   )
+}
+
+
+#' Create a Rstudio Project bootstraping a Satin Shiny App
+#'
+#' @param path path to create
+#' @param ... not used
+#' @export
+create_satin_app <- function(path, ...) {
+  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  dir.create(path = path)
+  from <- system.file("simple", package = "satin")
+  ll <- list.files(path = from, full.names = TRUE, all.files = TRUE)
+  file.copy(from = ll, to = path, overwrite = TRUE, recursive = TRUE)
 }
