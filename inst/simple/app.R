fa9a691b676c30e736eb6dd6a9c9e3203ce54354
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2019-01-31 05:16:27 -0800

    add project template

---
+++ inst/simple/app.R
@@ -0,0 +1,43 @@
+# Load Libraries -----
+library(shiny)
+library(shinydashboard)
+library(satin)
+
+# Load Utilities -----
+source_dirs('components')
+source_dirs('pages')
+
+# Global Data ----
+# This is passed to all page modules as an argument named data_global
+DATA <- list(
+  my_data = mtcars
+)
+
+# Configuration
+CONFIG <- yaml::read_yaml('_site.yml')
+
+# UI ----
+ui <- function(request){
+  dashboardPage(
+    # Header ----
+    dashboardHeader(title = CONFIG$name),
+
+    # Sidebar ----
+    dashboardSidebar(
+      st_create_sidebar(CONFIG, DATA)
+    ),
+
+    # Body -----
+    dashboardBody(
+      st_create_tab_items(CONFIG, DATA)
+    )
+  )
+}
+
+# Server -----
+server <- function(input, output, session){
+  st_call_all_modules(CONFIG, DATA)
+}
+
+# Run App ----
+shinyApp(ui = ui, server = server)
