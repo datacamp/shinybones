1bb4fbd13837755c376993f5d53fb626418795e9
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2019-03-13 22:07:56 -0700

    move sb_add_test to test.R

---
+++ R/test.R
@@ -0,0 +1,19 @@
+#' Add a test for a shiny module
+#'
+#' TODO: Automatically add a test file as testthat/test-shiny-modules.R
+#' @export
+#' @importFrom glue glue
+sb_add_test <- function(module_name){
+  tpl <- system.file('templates', 'test_module.R', package = 'shinybones')
+  if (!dir.exists('tests')){
+    stop("Please run usethis::use_testthat() to bootstrap tests", call. = FALSE)
+  }
+  test_dir <- file.path('tests', 'testthat', 'apps', module_name)
+  if (!dir.exists(test_dir)){
+    dir.create(test_dir, recursive = TRUE)
+  }
+  readLines(tpl, warn = FALSE) %>%
+    paste(collapse = "\n") %>%
+    glue::glue() %>%
+    cat(file = file.path(test_dir, 'app.R'))
+}
